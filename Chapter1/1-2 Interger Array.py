a4ef192c76ee6edece3784757f481b17f4b881a3
Sun Lee <66431424+iamsunghoonlee@users.noreply.github.com>
2023-09-09 09:23:00 -0400

    Chapter 1 Solution
    Integer Array

--- Chapter1/1-2 Interger Array.py
+++ Chapter1/1-2 Interger Array.py
@@ -7,15 +7,16 @@ def read_file(filepath : str):
 
 
 # 2. Counting Inversion Function
-def merge_and_count_split_inv(L1: list, L2: list, n: int):
+def merge_and_count_split_inv(L1: list, L2: list):
     # L1, L2 : sorted list / n = list length
     # List to store sorted integers
-    L = [0]*n
+    L = [0]*(len(L1) + len(L2))
 
     # i to track L1, j to track L2, k to track L
     i = 0
     j = 0
     k = 0
+    count = 0
 
     while i <= len(L1) - 1 and j <= len(L2) - 1:
         if L1[i] < L2[j]:
@@ -25,6 +26,7 @@ def merge_and_count_split_inv(L1: list, L2: list, n: int):
 
         elif L1[i] > L2[j]:
             L[k] = L2[j]
+            count += len(L1) - i
             j += 1
             k += 1
 
@@ -34,28 +36,33 @@ def merge_and_count_split_inv(L1: list, L2: list, n: int):
         k += 1
 
     while j <= len(L2) - 1:
-        L[k] = L1[j]
+        L[k] = L2[j]
         j += 1
         k += 1
 
-    return L
+    return L, count
 
 
 
-def sort_and_count(A: list, n: int):
-    if n == 1:
-        return 0
+def sort_and_count(A: list):
+    if len(A) == 1:
+        return A, 0
       
     else:
-        B, x = sort_and_count(A[(n//2):], (n/2))
-        C, y = sort_and_count(A[:(n//2)], (n/2))
-        split = merge_and_count_split_inv(B, C, n)
-    return 
+        mid = len(A)//2
+        
+        B, x = sort_and_count(A[:mid])
+        C, y  = sort_and_count(A[mid:])
+        D, z = merge_and_count_split_inv(B, C)
 
-# def main()
-#     read_file()
-#     count_inversion()
+    return D, x+y+z
+
+
+
+def main():
+    A = read_file("Chapter1\\1-2 Integer Array.txt")
+    print(sort_and_count(A)[1])
 
     
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
