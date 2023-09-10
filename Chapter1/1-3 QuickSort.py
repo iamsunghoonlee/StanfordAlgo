40409ac2d3793bd62832c37d2911977903b3fec7
Sun Lee <66431424+iamsunghoonlee@users.noreply.github.com>
2023-09-10 01:17:38 -0400

    Chapter1
    Update

--- Chapter1/1-3 QuickSort.py
+++ Chapter1/1-3 QuickSort.py
@@ -9,68 +9,107 @@ def read_file(filepath : str):
 
 # 2. Choose Pivot Fuction
 
-def choose_pivot(A: list, type: int):
-    if type == 1:
-        pivot = 0
-    
-    elif type == 2:
-        pivot = len(A)-1
-    
-    elif type == 3:
-        p1 = 0
-        if len(A)//2 == len(A)/2:
-            p2 = len(A)//2 - 1
-        else:
-            p2 = len(A)//2
-        p3 = len(A)-1
-        
-        # Getting Median among Three Candidates
-        if ((A[p1] < A[p2] and A[p2] < A[p3]) or (A[p3] < A[p2] and A[p2] < A[p1])) :
-            pivot = p2
-        elif ((A[p2] < A[p1] and A[p1] < A[p3]) or (A[p3] < A[p1] and A[p1] < A[p2])) :
-            pivot = p1
-        else :
-            pivot = p3
-        return pivot
+def choose_pivot_1(A: list):
+    pivot = 0
+    return pivot
+
+def choose_pivot_2(A: list):
+    pivot = len(A) - 1
+    return pivot
+
+def choose_pivot_3(A: list):    
+    p1 = 0
+    if len(A)//2 == len(A)/2:
+        p2 = len(A)//2 - 1
+    else:
+        p2 = len(A)//2
+    p3 = len(A)-1
+
+    # Getting Median among Three Candidates
+    if ((A[p1] < A[p2] and A[p2] < A[p3]) or (A[p3] < A[p2] and A[p2] < A[p1])) :
+        pivot = p2
+    elif ((A[p2] < A[p1] and A[p1] < A[p3]) or (A[p3] < A[p1] and A[p1] < A[p2])) :
+        pivot = p1
+    else :
+        pivot = p3
+    return pivot
 
 
 
 # 3. Partition Functions
 
-def partition(A: list, l: int, r: int, type: int):
-    # Swapping Chosen Pivot and the First Item
-    pivot = choose_pivot(A, type)
+def partition(A: list, pivot: int):
     A[0], A[pivot] = A[pivot], A[0]
-    
     p = A[0]
-    i = l+1
-    for j  in range(l+1, r+1):
+    
+    i, j = 1, 1
+    for j  in range(1, len(A)):
         if A[j] < p:
             A[i], A[j] = A[j], A[i]
             i += 1
-    A[l], A[i-1] = A[i-1], A[l]
-    return A
+    A[0], A[i-1] = A[i-1], A[0]
+    
+    return A, i
 
 
 # 4. Quick Sort Function
 
-def quick_sort(A: list, n: int):
-    if n == 1:
-        return A
+def quick_sort_1(A: list):
+    comp = len(A)-1
+    pivot = choose_pivot_1(A)
     
+    if len(A) <= 1:
+        return (A, 0)
     else:
-        pivot = choose_pivot(A, 1)
-        partition(A, )
-
-# def main():
-#     A = read_file("Chapter1\\1-3 QuickSort.txt")
-    
-#     # Problem 1
-    
-#     # Problem 2
-    
-#     # Problem 3 
+        A, i = partition(A, pivot)
+        A[:i-1], comp_l = quick_sort_1(A[:i-1])
+        A[i:], comp_r = quick_sort_1(A[i:])
+    return (A, comp + comp_l + comp_r)
 
 
-# if __name__ == '__main__':
-#     main()
+def quick_sort_2(A: list):
+    comp = len(A)-1
+    pivot = choose_pivot_2(A)
+    
+    if len(A) <= 1:
+        return (A, 0)
+    else:
+        A, i = partition(A, pivot)
+        A[:i-1], comp_l = quick_sort_2(A[:i-1])
+        A[i:], comp_r = quick_sort_2(A[i:])
+    return (A, comp + comp_l + comp_r)
+
+
+def quick_sort_3(A: list):
+    comp = len(A)-1
+    pivot = choose_pivot_3(A)
+    
+    if len(A) <= 1:
+        return (A, 0)
+    else:
+        A, i = partition(A, pivot)
+        A[:i-1], comp_l = quick_sort_3(A[:i-1])
+        A[i:], comp_r = quick_sort_3(A[i:])
+    return (A, comp + comp_l + comp_r)
+        
+
+def main():
+    A = read_file("Chapter1\\1-3 QuickSort.txt")
+    
+    # Problem 1
+    A1 = A.copy()
+    qs1 = quick_sort_1(A1)
+    print(qs1[1])
+    
+    # Problem 2
+    A2 = A.copy()
+    qs2 = quick_sort_2(A2)
+    print(qs2[1])
+    
+    # Problem 3 
+    A3 = A.copy()
+    qs3 = quick_sort_2(A3)
+    print(qs3[1])
+
+if __name__ == '__main__':
+    main()
